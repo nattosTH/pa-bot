c6a27faa17f35c2ec1b204b5699116f7ce1af99a
natto <natto@nattos-MacBook-Air.local>
2024-07-26 02:00:38 +0700

    implemented webhook

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,82 @@
-console.log("hello world!")
+import {
+  ClientConfig,
+  Client,
+  middleware,
+  MiddlewareConfig,
+  WebhookEvent,
+  TextMessage,
+  MessageAPIResponseBase,
+} from '@line/bot-sdk';
+import express, { Application, Request, Response } from 'express';
+import { load } from 'ts-dotenv';
+const env = load({
+  CHANNEL_ACCESS_TOKEN: String,
+  CHANNEL_SECRET: String,
+  PORT: Number,
+});
+
+
+
+const PORT = env.PORT || 3000;
+
+const config = {
+  channelAccessToken: env.CHANNEL_ACCESS_TOKEN || '',
+  channelSecret: env.CHANNEL_SECRET || '',
+};
+const clientConfig: ClientConfig = config;
+const middlewareConfig: MiddlewareConfig = config;
+
+// console.log(clientConfig);
+// console.log(middlewareConfig);
+
+const client = new Client(clientConfig); //①
+
+const app: Application = express(); //②
+
+app.get('/', async (_: Request, res: Response): Promise<Response> => { //③
+  return res.status(200).send({
+    message: 'success',
+  });
+});
+
+const textEventHandler = async ( //④
+  event: WebhookEvent
+): Promise<MessageAPIResponseBase | undefined> => {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return;
+  }
+
+  const { replyToken } = event;
+  const { text } = event.message;
+  const response: TextMessage = {
+    type: 'text',
+    text: text,
+  };
+  await client.replyMessage(replyToken, response);
+};
+
+app.post( //⑤
+  '/webhook',
+  middleware(middlewareConfig),
+  async (req: Request, res: Response): Promise<Response> => {
+    const events: WebhookEvent[] = req.body.events;
+    console.log(req.body)
+    await Promise.all(
+      events.map(async (event: WebhookEvent) => {
+        try {
+          await textEventHandler(event);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            console.error(err);
+          }
+          return res.status(500).end();
+        }
+      })
+    );
+    return res.status(200).end()
+  }
+);
+
+app.listen(PORT, () => { //⑥
+  console.log(`http://localhost:${PORT}/`);
+});
